9c2899bfaf6cdf5ce13d7c19580b20712b7fbecc
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-04-02 03:09:27 +0200

    test

--- parse-config.sh
+++ parse-config.sh
@@ -3,7 +3,7 @@
 #
 # check wether dashlord.yml or urls.txt exist and output a proper list of urls as JSON
 #
-function parseConfig() {
+parseConfig() {
     if [[ -e "dashlord.yaml" ]]
     then
         # echo "parse dashlord.yaml"
